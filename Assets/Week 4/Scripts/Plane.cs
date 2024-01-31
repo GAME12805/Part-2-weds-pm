635a3b3f06691e9890047f8c75ca1270a93556c2
SHERNET\barryki <kit.barry@sheridancollege.ca>
2024-01-31 14:59:07 -0500

    Task 2 complete: aircraft moves and lands

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Plane : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class Plane : MonoBehaviour
     Vector2 currentPosition;
     Rigidbody2D rigidbody;
     public float speed = 1;
+    public AnimationCurve landing;
+    float landingTimer;
 
     void Start()
     {
@@ -35,6 +38,17 @@ public class Plane : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            landingTimer += 0.5f * Time.deltaTime;
+            float interpolation = landing.Evaluate(landingTimer);
+            if(transform.localScale.z < 0.1f)
+            {
+                Destroy(gameObject);
+            }
+            transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, interpolation);
+        }
+
         lineRenderer.SetPosition(0, transform.position);
         if(points.Count > 0 )
         {
