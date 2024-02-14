6e7112cd2e407b98e4d859f06b8eb938837c4dc4
SHERNET\barryki <kit.barry@sheridancollege.ca>
2024-02-14 13:27:17 -0500

    Week 5

--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -13,6 +13,7 @@ public class Knight : MonoBehaviour
     public float health;
     public float maxHealth = 5;
     bool isDead = false;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -43,14 +44,15 @@ public class Knight : MonoBehaviour
     {
         if (isDead) return;
         clickingOnSelf = true;
-        TakeDamage(1);
+        SendMessage("TakeDamage", 1);
+        
     }
     private void OnMouseUp()
     {
         clickingOnSelf = false;
     }
 
-    void TakeDamage(float damage)
+    public void TakeDamage(float damage)
     {
         health -= damage;
         health = Mathf.Clamp(health, 0, maxHealth);
